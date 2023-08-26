9a06847ed02dc3707656ed027108a5f555248e00
someshwar reddy <somudasari8@gmail.com>
2023-08-26 18:47:18 +0530

    new change.

--- Address-app/src/main/java/com/somesh/AddressAppApplication.java
+++ Address-app/src/main/java/com/somesh/AddressAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AddressAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(AddressAppApplication.class, args);
 	}
 
